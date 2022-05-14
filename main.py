4a67e14d427cbfba277b1a44fa500984a3b17616
iprogrammyou <p.hueber@protonmail.com>
2022-05-15 00:44:13 +0200

    mostly ready, but still a problem with player name utf boom

--- main.py
+++ main.py
@@ -1,51 +1,72 @@
-from mctools import RCONClient, PINGClient, QUERYClient
-import os
-import json
-import time as time_module
-import dotenv
+from mctools import RCONClient, PINGClient
+from dataclasses import dataclass
 from getpass import getpass
+from time import sleep
+import dotenv
+import json
+import os
 
 dotenv.load_dotenv()
 
 HOST = os.getenv('RCON_HOST') or '185.208.204.244'
 PORT = os.getenv('RCON_HOST') or 25575
-PASW = os.getenv('RCON_PASSWORD') or getpass('rcon password: ')
-
-rcon = RCONClient(HOST, port=PORT)
-ping = PINGClient(HOST, proto_num=0)
-time = time_module.localtime()
-day = time[6] + 1
-json_file = 'player_info.json'
-testtext = 'test'
-
-with open(json_file, mode='r') as read_file:
-    data = json.load(read_file)
+RCON_PASSWORD = os.getenv('RCON_PASSWORD') or getpass('rcon password: ')
+REFRESH_TIME = 5
+WEEKDAY_PLAYTIME = 60
 
 
-dict_of_players_with_time = {'time': time}
+@dataclass
+class Player:
+    uid: str
+    name: str
 
-stats = ping.get_stats()
-config_players = stats.get('players')
-players = config_players.get('sample')
-if players:
-    for player_info in players:
-        player_id = player_info[1]
-        if player_id in dict_of_players_with_time:
-            print('seems to function')
-        elif player_id is not dict_of_players_with_time:
-            if day <= 5:
-                dict_of_players_with_time[player_id] = {'time_left': 120}
+
+def load_json(path: str) -> dict:
+    with open(path, mode='r+') as f:
+        data = json.load(f)
+    return data
+
+
+def dump_json(path: str, data):
+    with open(path, mode='w+') as f:
+        json.dump(obj=data, fp=f)
+
+
+def kick_player(rcon: RCONClient, player: Player):
+    print(f'kicking player{player.name}')
+    rcon.command(f'kick {player.name}')
+
+
+def connected_players(ping: PINGClient) -> list[Player]:
+    playerlist = []
+    stats = ping.get_stats()
+    config_players = stats.get('players')
+    players = config_players.get('sample')
+    if players:
+        for player_info in players:
+            new_player = Player(uid=player_info[1], name=player_info[0])
+            playerlist.append(new_player)
+    return playerlist
+
+
+while True:
+    ping = PINGClient(HOST, proto_num=0)
+    rcon = RCONClient(HOST, port=PORT)
+    if not rcon.login(RCON_PASSWORD):
+        raise ValueError('rcon login geht nicht')
+    json_path = 'data/player_info.json'
+
+    loaded_file = load_json(json_path)
+    for player in connected_players(ping):
+        if player.uid in loaded_file.keys():
+            if loaded_file[player.uid] < WEEKDAY_PLAYTIME:
+                old_time = loaded_file[player.uid]
+                loaded_file[player.uid] = old_time + REFRESH_TIME
+                dump_json(json_path, loaded_file)
             else:
-                dict_of_players_with_time[player_id] = {'time_left': 240}
-    print(dict_of_players_with_time)
-else:
-    dict_of_players_with_time['test'] = testtext
-    print(dict_of_players_with_time)
+                kick_player(rcon, player)
+        else:
+            loaded_file[player.uid] = REFRESH_TIME
+            dump_json(json_path, loaded_file)
 
-
-# if rcon.login(PASW):
-#     resp = rcon.command('op spoderman&boman')
-#     print(resp)
-#     connected = rcon.is_connected
-#     print(connected)
-
+    sleep(REFRESH_TIME)
